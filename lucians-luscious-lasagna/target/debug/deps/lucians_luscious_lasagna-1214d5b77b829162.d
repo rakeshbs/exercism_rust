ce5ca9bc9a3fc331fa0cb7aa5c11404defce7944
rakeshbs <rakeshbs@gmail.com>
2022-05-15 13:17:44 +0530

    Added gitignore

--- lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-1214d5b77b829162.d
+++
@@ -1,5 +0,0 @@
-/Users/rakeshbs/Projects/Exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-1214d5b77b829162.rmeta: src/lib.rs
-
-/Users/rakeshbs/Projects/Exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-1214d5b77b829162.d: src/lib.rs
-
-src/lib.rs:
--- lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-1df5c87ded8b903c.d
+++
@@ -1,7 +0,0 @@
-/Users/rakeshbs/Projects/Exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-1df5c87ded8b903c.rmeta: src/lib.rs
-
-/Users/rakeshbs/Projects/Exercism/rust/lucians-luscious-lasagna/target/debug/deps/liblucians_luscious_lasagna-1df5c87ded8b903c.rlib: src/lib.rs
-
-/Users/rakeshbs/Projects/Exercism/rust/lucians-luscious-lasagna/target/debug/deps/lucians_luscious_lasagna-1df5c87ded8b903c.d: src/lib.rs
-
-src/lib.rs:
