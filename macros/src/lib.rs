c075f6d3b1fc4525a9d682c229bd56e3fe45a507
rakeshbs <rakeshbs@gmail.com>
2022-05-20 20:39:16 +0530

    Finished macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! hashmap {
+    ($($key:expr=>$value:expr,)*) => {
+        {
+            use ::std::collections::HashMap;
+            let mut hm = HashMap::new();
+            $(
+                hm.insert($key, $value);
+            )*
+            hm
+        }
+    };
+    ($($key:expr=>$value:expr),*) => {{
+        macros::hashmap!($($key => $value,)*)
+    }};
+}
